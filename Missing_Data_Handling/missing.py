d53c525391381f6eec3d0ef15aabd9a772ca9e06
VivekXploit <dhoblevivek23@gmail.com>
2026-02-10 12:35:32 +0530

    Implement missing data handling example in missing.py
    Added example code to demonstrate handling missing data in a DataFrame using pandas.

--- Missing_Data_Handling/missing.py
+++ Missing_Data_Handling/missing.py
@@ -1,1 +1,26 @@
+"""
+NaN (Not a Number)
+None(for object data type)
 
+isnull() method wich detects null values in dataframe and return boolean dataframe 
+True -> values are missing
+False -> values are present
+
+"""
+
+import pandas as pd
+
+data = {
+    "Name":["Ram", None, "Dhansham", "Ghansham", "Aditi", "Jagdish","Raj", "Sitaram"], 
+    "Age":[28,None,22,30,29,40,25,32],
+    "Salary":[50000,None,45000,52000,49000,70000,48000,58000],
+    "Performance Score":[85,None,78,92,88,95,80,89]
+}
+
+df = pd.DataFrame(data)
+
+print("Dataframe which contains null values...")
+print(df.isnull())
+
+print("\nColumns Containing Null Values...")
+print(df.isnull().sum())
